4d4208e8858116a652e703436733a0e358345904
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-15 23:40:37 +0900

    Feat: 요청 종류에 따라 form 인스턴스 생성 시 data 전달 결정

--- pystagram/users/views.py
+++ pystagram/users/views.py
@@ -1,8 +1,17 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
 from users.forms import LoginForm
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("/posts/feeds/")
-    return render(request, "users/login.html", context)
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        print("form.is_valid():", form.is_valid())
+        print("form.cleaned_data:", form.cleaned_data)
+        context = {"form": form}
+        return render(request, "users/login.html", context)
+    else:
+        form = LoginForm()
+        context = {"form": form}
+        return render(request, "users/login.html", context)
