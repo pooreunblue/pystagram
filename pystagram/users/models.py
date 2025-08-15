d9b15598458c06ef89c979b888c9c7564045de61
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-15 23:24:17 +0900

    Feat: CustomUser 필드 추가

--- pystagram/users/models.py
+++ pystagram/users/models.py
@@ -2,4 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    pass
+    profile_image = models.ImageField(
+        "프로필 이미지", upload_to="users/profile", blank=True)
+    short_description = models.TextField("소개글", blank=True)
