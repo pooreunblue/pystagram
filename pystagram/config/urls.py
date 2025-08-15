49e9effb43f44ab5e311398c94c0f828aae62791
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-15 23:28:28 +0900

    Feat: 새 urls.py Root URLconf에 등록

--- pystagram/config/urls.py
+++ pystagram/config/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import users
 from config.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("users/", include("users.urls")),
     path("", index),
 ]
 
