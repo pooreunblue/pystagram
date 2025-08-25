54ae01b312fbbe4d13f24496040510dc7497b7b6
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-25 21:12:38 +0900

    Feat: admin 구현

---
+++ pystagram/posts/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from posts.models import Post, PostImage, Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content",
+    ]
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "photo",
+    ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content",
+    ]
