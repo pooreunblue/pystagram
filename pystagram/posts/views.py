9b7d0426a91d7d93dd684191d14bd4353ef036bd
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-30 18:46:51 +0900

    Feat: View 함수 내부에서 사용자 정보 Comment 생성 시 직접 할당

--- pystagram/posts/views.py
+++ pystagram/posts/views.py
@@ -19,4 +19,21 @@ def feeds(request):
 
 @require_POST # 댓글 작성을 처리할 View, Post 요청만 허용한다
 def comment_add(request):
-        print(request.POST)
+        # request.POST러 전달된 데이터를 사용해 CommentForm 인스턴스를 생성
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            # commit=False 옵션으로 메모리상에 Comment 객체 생성
+            comment = form.save(commit=False)
+
+            # Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
+            comment.user = request.user
+
+            # DB에 Comment 객체 저장
+            comment.save()
+
+            # 생성된 Comment의 정보 확인
+            print(comment.id)
+            print(comment.content)
+            print(comment.user)
+            # 생성 완료 후에는 피드 페이지로 다시 이동
+            return redirect("/posts/feeds/")
