61be94c81f52b2b1c8f6cb068ca01e0019ad3210
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-02 10:48:00 +0900

    Feat: 글의 댓글 수 표시

--- pystagram/posts/views.py
+++ pystagram/posts/views.py
@@ -1,5 +1,5 @@
 from posts.forms import CommentForm
-from posts.models import Post
+from posts.models import Post, Comment
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from django.http import HttpResponseRedirect
@@ -20,21 +20,28 @@ def feeds(request):
 
 @require_POST # 댓글 작성을 처리할 View, Post 요청만 허용한다
 def comment_add(request):
-        # request.POST러 전달된 데이터를 사용해 CommentForm 인스턴스를 생성
-        form = CommentForm(data=request.POST)
-        if form.is_valid():
-            # commit=False 옵션으로 메모리상에 Comment 객체 생성
-            comment = form.save(commit=False)
+    # request.POST러 전달된 데이터를 사용해 CommentForm 인스턴스를 생성
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # commit=False 옵션으로 메모리상에 Comment 객체 생성
+        comment = form.save(commit=False)
 
-            # Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
-            comment.user = request.user
+        # Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
+        comment.user = request.user
 
-            # DB에 Comment 객체 저장
-            comment.save()
+        # DB에 Comment 객체 저장
+        comment.save()
 
-            # 생성된 Comment의 정보 확인
-            print(comment.id)
-            print(comment.content)
-            print(comment.user)
-            # 생성 완료 후에는 피드 페이지로 다시 이동
-            return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+        # 생성된 Comment의 정보 확인
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+        # 생성 완료 후에는 피드 페이지로 다시 이동
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+
+@require_POST
+def comment_delete(request, comment_id):
+    if request.method == 'POST':
+        comment = Comment.objects.get(id=comment_id)
+        comment.delete()
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
