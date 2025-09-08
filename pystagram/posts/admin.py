30fc17a94eae0b3b51fb3706245705222a49cd14
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-08 09:38:48 +0900

    Feat: HashTag admin 구현

--- pystagram/posts/admin.py
+++ pystagram/posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from posts.models import Post, PostImage, Comment
+from posts.models import Post, PostImage, Comment, Hashtag
 import admin_thumbnails
 
 class CommentInline(admin.TabularInline):
@@ -38,3 +38,7 @@ class CommentAdmin(admin.ModelAdmin):
         "post",
         "content",
     ]
+
+@admin.register(Hashtag)
+class HashTagAdmin(admin.ModelAdmin):
+    pass
