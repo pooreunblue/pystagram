91277ff1ecabc726eca4b3ca372471e1b2fea78b
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-10 16:51:22 +0900

    Feat: PostAdmin에 like_users 추가

--- pystagram/posts/admin.py
+++ pystagram/posts/admin.py
@@ -14,6 +14,15 @@ class PostImageInline(admin.TabularInline):
     model = PostImage
     extra = 1
 
+class LikeUserInline(admin.TabularInline):
+    model = Post.like_users.through
+    verbose_name = "좋아요 한 User"
+    verbose_name_plural = f"{verbose_name} 목록"
+    extra = 1
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
@@ -24,6 +33,7 @@ class PostAdmin(admin.ModelAdmin):
     inlines = [
         CommentInline,
         PostImageInline,
+        LikeUserInline,
     ]
 
     formfield_overrides = {
