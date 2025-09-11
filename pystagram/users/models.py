423e1898ce88681fc3171204aa286081495aa7f4
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-11 11:24:17 +0900

    Feat: 팔로우 관계 모델로 중개 테이블 생성

--- pystagram/users/models.py
+++ pystagram/users/models.py
@@ -13,3 +13,21 @@ class User(AbstractUser):
     )
     def __str__(self):
         return self.username
+
+class Relationship(models.Model):
+    from_user = models.ForeignKey(
+        "users.User",
+        verbose_name="팔로우를 요청한 사용자",
+        related_name="following_relationships",
+        on_delete=models.CASCADE,
+    )
+    to_user = models.ForeignKey(
+        "users.User",
+        verbose_name="팔로우 요청의 대상",
+        related_name="follower_relationships",
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"관계 ({self.from_user} -> {self.to_user})"
