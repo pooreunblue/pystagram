a38c643182a3dc46842533f94a7523b64f3a5f08
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-12 12:03:24 +0900

    Feat: 팔로우, 팔로잉 URL 구설

--- pystagram/users/urls.py
+++ pystagram/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.views import login_view, logout_view, signup, profile
+from users.views import login_view, logout_view, signup, profile, followers, following
 
 app_name = "users"
 urlpatterns=[
@@ -7,4 +7,6 @@ urlpatterns=[
     path("logout/", logout_view, name="logout"),
     path("signup/", signup, name="signup"),
     path("<int:user_id>/profile/", profile, name="profile"),
+    path("<int:user.id>/followers/", followers, name="followers"),
+    path("<int:user.id>/following/", following, name="following"),
 ]
